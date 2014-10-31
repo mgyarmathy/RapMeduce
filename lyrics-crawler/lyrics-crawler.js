b786045b33f1c0c982e305ca6b106f8e755a60ef
Michael Gyarmathy <mgyarmathy2011@gmail.com>
2014-10-30 19:12:33 -0500

    making some progress...

--- lyrics-crawler/lyrics-crawler.js
+++ lyrics-crawler/lyrics-crawler.js
@@ -1,26 +1,45 @@
 var Crawler = require('crawler');
 var Set = require('collections/set');
+var lineReader = require('line-reader');
 
-var onFetch = function(error, result, $) {
+function Lyric(artist, song, line) {
+	this.artist = artist;
+	this.song = song;
+	this.line = line;
+}
+
+var c = new Crawler({
+
+'callback': function(error, result, $) {
 	if ($('#songLyricsDiv').length) {
+		var songInfo = $('.pagetitle h1').text().replace(' Lyrics','').split(' - ');
+		var artist = songInfo[0];
+		var song = songInfo[1];
 		var lines = $('#songLyricsDiv').text().split('\n');
 		var formatted_lines = [];
 		for (var i = 0; i<lines.length; i++) {
 			lines[i] = lines[i].replace('\r', '');
-			if (lines[i].length > 0 && lines[i].charAt(0) != '[') formatted_lines.push(lines[i]);
+			if (lines[i].length > 0 && lines[i].charAt(0) != '[') {
+				formatted_lines.push(new Lyric(artist, song, lines[i]));
+			}
 		}
-		console.log(new Set(formatted_lines).toArray());
+		var s = new Set(formatted_lines);
+		s.forEach(function(lyric){
+			console.log(JSON.stringify(lyric));
+		});
 	} else if ($('.tracklist').length){
 		$($('.tracklist').get(0)).find('td a').each(function() {
+			console.log('adding to queue: '+$(this).attr('href'));
 			c.queue($(this).attr('href'));
 		});
 	}
-};
-
-var c = new Crawler({
-'callback': onFetch,
+},
 'onDrain': function() { process.exit(0) }
 });
 
-// Queue just one URL, with default callback
-c.queue("http://www.songlyrics.com/drake-lyrics/");
+// Queue just artist URLS from -- http://en.wikipedia.org/wiki/List_of_hip_hop_musicians
+lineReader.eachLine('../data/rap-artists-test.txt', function(line, last) {
+	var artistUrl = 'http://www.songlyrics.com/'+line.toLowerCase().split(' ').join('-')+'-lyrics/';
+	console.log(artistUrl);
+	c.queue({uri: artistUrl, priority: 6});
+});
