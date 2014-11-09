0b005d91e82672fe7cbe369a25ed27ecde02fedb
Michael Gyarmathy <mgyarmathy2011@gmail.com>
2014-11-09 14:46:50 -0600

    added up/down animations and ability to swap out lines with alternates

--- app/js/app.js
+++ app/js/app.js
@@ -3,6 +3,37 @@ var Lyric = function(artist, song, line) {
     self.artist = ko.observable(artist);
     self.song = ko.observable(song);
     self.line = ko.observable(line);
+    // have to use JSON objects here instead of Lyric() to prevent infinite loop
+    self.alternateLines = ko.observableArray([
+        {artist: 'Kanye West', song: 'Power', line: 'I\'m Livin in that 21st Century, doing something mean to it' },
+        {artist: 'Kanye West', song: 'Power', line: 'Do it better than anybody you ever seen do it'},
+        {artist: 'Kanye West', song: 'Power', line: 'Screams from the haters, got a nice ring to it'},
+        {artist: 'Kanye West', song: 'Power', line: 'I guess every superhero need his theme music'},
+    ]);
+
+    self.alternateLineUp = function(data, event) {
+        event.target.classList.remove('animated', 'fadeInUp', 'fadeInDown');
+        setTimeout(function() {
+            self.alternateLines().push({artist: self.artist(), song: self.song(), line: self.line()})
+            var lyric = self.alternateLines().shift();
+            self.artist(lyric.artist);
+            self.song(lyric.song);
+            self.line(lyric.line);
+            event.target.classList.add('animated', 'fadeInUp');
+        },10);
+    };
+
+    self.alternateLineDown = function(data, event) {
+        event.target.classList.remove('animated', 'fadeInUp', 'fadeInDown');
+        setTimeout(function() {
+            self.alternateLines().unshift({artist: self.artist(), song: self.song(), line: self.line()});
+            var lyric = self.alternateLines().pop();
+            self.artist(lyric.artist);
+            self.song(lyric.song);
+            self.line(lyric.line);
+            event.target.classList.add('animated', 'fadeInDown');
+        },10);
+    };
 }
 
 function AppViewModel() {
@@ -24,19 +55,19 @@ function AppViewModel() {
 
     self.changeActiveLine = function(index, data, event) {
         self.activeLine(index);
-    }
+    };
 
     self.generateLine = function(index, data, event) {
         self.lines.splice(index+1, 0, new Lyric('Kanye West', 'Power', 'No one man should have all that power'));
         self.changeActiveLine(index+1, data, event);
-    }
+    };
 
     self.deleteLine = function(index, data, event) {
         self.lines.splice(index, 1);
         if (self.activeLine() >= self.lines().length) {
             self.changeActiveLine(self.lines().length - 1);
         }
-    }
+    };
 };
 
 ko.applyBindings(new AppViewModel());
