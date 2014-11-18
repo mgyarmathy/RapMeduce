7ec0fe9332caa05a0eca44a9ba2d5cf9ca72526d
Michael Gyarmathy <mgyarmathy2011@gmail.com>
2014-11-18 17:03:16 -0600

    convert tabs to spaces in RhymeScore module

--- web-service/RhymeScore.py
+++ web-service/RhymeScore.py
@@ -17,8 +17,8 @@ def stopword(a_list_of_words):
     return filteredtext
 
 def stemming(a_list_of_words):
-	stemmed = [stem(words) for words in a_list_of_words]
-	return stemmed
+    stemmed = [stem(words) for words in a_list_of_words]
+    return stemmed
 
 def jaccard_similarity(line1_tokens, line2_tokens):
     a = set(line1_tokens)
@@ -26,14 +26,14 @@ def jaccard_similarity(line1_tokens, line2_tokens):
     return (float(len(a & b)) / float(len (a|b)))
 
 def score(user_line, rhyme_line, rhyme_line_syllables, freq):
-	user_line_tokens = stemming(stopword(tokenize(user_line)))
-	rhyme_line_tokens = stemming(stopword(tokenize(rhyme_line)))
+    user_line_tokens = stemming(stopword(tokenize(user_line)))
+    rhyme_line_tokens = stemming(stopword(tokenize(rhyme_line)))
 
-	# calculate user_line syllables
-	user_line_syllables = 0
-	for token in tokenize(user_line):
-		user_line_syllables += CountSyllables(token)
+    # calculate user_line syllables
+    user_line_syllables = 0
+    for token in tokenize(user_line):
+        user_line_syllables += CountSyllables(token)
 
-	similarity = jaccard_similarity(user_line_tokens, rhyme_line_tokens)
+    similarity = jaccard_similarity(user_line_tokens, rhyme_line_tokens)
 
-	return (_sim*similarity) + (_f*(1.0/(float(freq)/34))) + (_syl*(1/float(abs(rhyme_line_syllables-user_line_syllables) + 0.01)))
+    return (_sim*similarity) + (_f*(1.0/(float(freq)/34))) + (_syl*(1/float(abs(rhyme_line_syllables-user_line_syllables) + 0.01)))
